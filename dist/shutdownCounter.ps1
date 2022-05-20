03bf8aeaae7ac93db935f020e8689bae58596b88
FrancisJeon <jsw00827@gmail.com>
2022-05-20 15:32:59 +0900

    first distribution

---
+++ dist/shutdownCounter.ps1
@@ -0,0 +1,24 @@
+﻿# v 1.0.0
+write-host "`n종료시간을 입력해주세요. 예) 20220520 1500" -ForegroundColor Yellow
+write-host "`n주의: 현재 시간보다 과거 입력시 컴퓨터가 즉시 종료됩니다. 저장하지 않은 문서가 날아갈 수 있음" -ForegroundColor Red
+write-host "작업중인 문서는 꼭 저장하고 사용하세요`n" -ForegroundColor Red
+
+$input=Read-host "입력값: "
+$end = [Datetime]::ParseExact($input, 'yyyyMMdd HHmm', $null)
+$endStr = $end.ToString("yyyy년 MM월 dd일 HH시 mm분")
+
+while ($true) {
+    clear
+    if ((Get-Date) -ge $end) {
+        Stop-Computer -ComputerName localhost -Force
+        return
+    }
+    else {
+        write-host "================================================================================================`n"
+        write-host "`t`t종료 시간 : $endStr" -ForegroundColor Red
+        $remain = New-TimeSpan -Start (Get-Date) -End $end
+        write-host "`t`t남은시간:" $remain.Days"일"$remain.Hours"시간"$remain.Minutes"분"$remain.Seconds"초 (취소하려면 Ctrl+C를 눌러주세요)" -ForegroundColor Yellow
+        write-host "`n================================================================================================"
+    }
+    Start-Sleep 1
+}
---
+++ powershell/timer/powershell.bat
@@ -0,0 +1,3 @@
+@echo off
+Powershell.exe -noprofile -executionpolicy bypass -file %cd%\shutdownCounter.ps1
+pause
