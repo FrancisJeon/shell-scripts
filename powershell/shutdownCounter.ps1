ae494c3e6fad1dba8b66ae9f341a194672289093
FrancisJeon <jsw00827@gmail.com>
2022-05-20 14:33:08 +0900

    ParseExact로 입력 양식 제한해서 종료시간 받아왔고 time format도 정상적으로 맞춰진 상태 지금은 작동한다

--- powershell/shutdownCounter.ps1
+++ powershell/shutdownCounter.ps1
@@ -1,33 +1,29 @@
-﻿write-host "원하시는 종료시간을 입력해주세요. 예) 20220520 1500"
-# write-host "다음 예시의 입력값은 모두 동일한 값입니다. 편한 포맷으로 사용하세요" -ForegroundColor Yellow
+﻿# write-host "다음 예시의 입력값은 모두 동일한 값입니다. 편한 포맷으로 사용하세요" -ForegroundColor Yellow
 # write-host "원하는 종료시간 = 2022년 05월 20일 13시00분00초" -ForegroundColor Yellow
 # write-host "`t예시1: 052022 1300" -ForegroundColor Yellow
 # write-host "`t예시2: 052022 130000" -ForegroundColor Yellow
 # write-host "`t예시3: 05 20 22 13 00 00" -ForegroundColor Yellow
 # write-host "`t예시4: 0520 22 1300" -ForegroundColor Yellow
-$start = (get-date)
-$input=Read-host "종료 시간을 입력하세요"
-$end = [Datetime]::ParseExact($input, 'yyyyMMdd hhmm', $null)
+write-host "종료시간을 입력해주세요. 예) 20220520 1500"
+$input=Read-host "입력값: "
+$end = [Datetime]::ParseExact($input, 'yyyyMMdd HHmm', $null)
 
-echo $start
-echo $end
-
-# while ($true)
-# {
+while ($true)
+{
 # clear
-# if ((Get-Date) -ge $end)
-# {
-#     # Stop-Computer -ComputerName localhost -Force
-#     write-host "종료합니다"
-# return
-# }
-# else
-# {
-#     write-host "================================================================================================`n"
-#     write-host "`t`t종료 예정 시간 : $end" -ForegroundColor Red
-#     $remain = New-TimeSpan -Start (Get-Date) -End $end
-#     write-host "`t`t남은시간:" $remain.Hours"시"$remain.Minutes"분"$remain.Seconds"초 (취소하려면 Ctrl+C를 눌러주세요)" -ForegroundColor Yellow
-#     write-host "`n================================================================================================"
-# }
-# Start-Sleep 1
-# }
+if ((Get-Date) -ge $end)
+{
+    # Stop-Computer -ComputerName localhost -Force
+    write-host "종료합니다"
+return
+}
+else
+{
+    write-host "================================================================================================`n"
+    write-host "`t`t종료 예정 시간 : $end" -ForegroundColor Red
+    $remain = New-TimeSpan -Start (Get-Date) -End $end
+    write-host "`t`t남은시간:" $remain.Hours"시"$remain.Minutes"분"$remain.Seconds"초 (취소하려면 Ctrl+C를 눌러주세요)" -ForegroundColor Yellow
+    write-host "`n================================================================================================"
+}
+Start-Sleep 1
+}
